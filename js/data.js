fd80f78cdb9cb9741c45977174d8b67848d3a697
Laura Smith <LSmith@planetart.com>
2026-07-21 18:54:56 -0700

    Add screenshot pairs 06-23 for translation review

--- js/data.js
+++ js/data.js
@@ -53,6 +53,150 @@ window.SCREENSHOTS = [
     imageDe: "images/05-pbpcu-de.png",
     germanTexts: [
     ],
+  },
+  {
+    id: "06-fpa-pcu",
+    title: "Fpa Pcu",
+    imageEn: "images/06-fpa-pcu-us.png",
+    imageDe: "images/06-fpa-pcu-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "07-pt-pcu",
+    title: "Pt Pcu",
+    imageEn: "images/07-pt-pcu-us.png",
+    imageDe: "images/07-pt-pcu-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "08-pb-mcspace",
+    title: "Pb Mcspace",
+    imageEn: "images/08-pb-mcspace-us.png",
+    imageDe: "images/08-pb-mcspace-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "09-pt-mcspace",
+    title: "Pt Mcspace",
+    imageEn: "images/09-pt-mcspace-us.png",
+    imageDe: "images/09-pt-mcspace-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "10-fpa-mcspace",
+    title: "Fpa Mcspace",
+    imageEn: "images/10-fpa-mcspace-us.png",
+    imageDe: "images/10-fpa-mcspace-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "11-pb-home-tab-unredeemed",
+    title: "Pb Home Tab Unredeemed",
+    imageEn: "images/11-pb-home-tab-unredeemed-us.png",
+    imageDe: "images/11-pb-home-tab-unredeemed-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "12-pt-home-tab-unredeemed",
+    title: "Pt Home Tab Unredeemed",
+    imageEn: "images/12-pt-home-tab-unredeemed-us.png",
+    imageDe: "images/12-pt-home-tab-unredeemed-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "13-fpa-home-tab-unredeemed",
+    title: "Fpa Home Tab Unredeemed",
+    imageEn: "images/13-fpa-home-tab-unredeemed-us.png",
+    imageDe: "images/13-fpa-home-tab-unredeemed-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "14mydeals1",
+    title: "14mydeals1",
+    imageEn: "images/14mydeals1-us.png",
+    imageDe: "images/14mydeals1-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "15mydeal2",
+    title: "15mydeal2",
+    imageEn: "images/15mydeal2-us.png",
+    imageDe: "images/15mydeal2-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "16mydeals3",
+    title: "16mydeals3",
+    imageEn: "images/16mydeals3-us.png",
+    imageDe: "images/16mydeals3-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "17mydeals4",
+    title: "17mydeals4",
+    imageEn: "images/17mydeals4-us.png",
+    imageDe: "images/17mydeals4-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "18mydeals5",
+    title: "18mydeals5",
+    imageEn: "images/18mydeals5-us.png",
+    imageDe: "images/18mydeals5-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "19productinfo",
+    title: "19productinfo",
+    imageEn: "images/19productinfo-us.png",
+    imageDe: "images/19productinfo-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "20info",
+    title: "20info",
+    imageEn: "images/20info-us.png",
+    imageDe: "images/20info-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "21info",
+    title: "21info",
+    imageEn: "images/21info-us.png",
+    imageDe: "images/21info-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "22info",
+    title: "22info",
+    imageEn: "images/22info-us.png",
+    imageDe: "images/22info-de.png",
+    germanTexts: [
+    ],
+  },
+  {
+    id: "23info",
+    title: "23info",
+    imageEn: "images/23info-us.png",
+    imageDe: "images/23info-de.png",
+    germanTexts: [
+    ],
   }
 ];
 
